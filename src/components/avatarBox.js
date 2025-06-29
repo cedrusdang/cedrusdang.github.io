d577f5bb6ca24cd9405ad776ce937966d51b82e2
Cedrus Dang <dtt161094@gmail.com>
2025-06-30 03:15:10 +0800

    finish the build, update ver to 1.0, and fix the meta info files

--- src/components/avatarBox.js
+++ src/components/avatarBox.js
@@ -8,7 +8,7 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import Box from '@mui/material/Box';
 
 import StyledPaper from './styledPaper.js';
-import {ownerName, GitHubURL, LinkedInURL, email} from '../data/info.js';
+import {ownerName, GitHubURL, LinkedInURL} from '../data/info.js';
 import { LeftBoxIntro } from '../data/intro.js';
 
 const AvatarPic = "/images/avatar.jpg"
